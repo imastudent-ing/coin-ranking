3eb2a5b3dab34c2c6ad75075702c897cc9acde2d
student <o.siminao.o@gmail.com>
2026-03-08 20:38:21 +0900

    Create cron.js

---
+++ api/cron.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+  const SUPABASE_URL = process.env.SUPABASE_URL;
+  const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY;
+
+  const COINS = [
+    { addr:"5C7nFDCgWTLnHgsz​​​​​​​​​​​​​​​​
